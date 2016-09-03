674e6635c2853d5fe5cc987d9ece77fdcf4fced5
Samridh <samridh@purestorage.com>
2016-09-02 17:35:48 -0700

    Create Volume-Manager Seed

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'volume-manager',
+    template: `<h1>Pure Volume Manager</h1>`
+})
+export class AppComponent {}
