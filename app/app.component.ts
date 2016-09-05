8dcce7f95f93c85115cced568c9ccab426d6e1a2
Samridh <samridh@purestorage.com>
2016-09-04 18:18:50 -0700

    Part4: Services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Volume } from './volume';
+import { VolumeService } from './volume.service';
 
 @Component({
     moduleId: module.id,
@@ -30,31 +32,25 @@ import { Volume } from './volume';
         </li>
     </ul>
     <volume-detail [volume]="selectedVolume"></volume-detail>
-    `
+    `,
+    providers: [VolumeService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Pure Volume Manager';
-    volume: Volume = {
-        name: 'vol1',
-        size: '100G'
-    };
-    volumes = VOLUMES;
+    volumes: Volume[];
     selectedVolume: Volume;
+
+    constructor(private volumeService: VolumeService) {}
+
+    ngOnInit(): void {
+        this.getVolumes();
+    }
 
     onSelect(volume: Volume): void {
         this.selectedVolume = volume;
     }
-}
 
-const VOLUMES: Volume[] = [
-    { name: 'vol1', size: '100G' },
-    { name: 'vol2', size: '101G' },
-    { name: 'vol3', size: '102G' },
-    { name: 'vol4', size: '103G' },
-    { name: 'vol5', size: '104G' },
-    { name: 'vol6', size: '105G' },
-    { name: 'vol7', size: '106G' },
-    { name: 'vol8', size: '107G' },
-    { name: 'vol9', size: '108G' },
-    { name: 'vol10', size: '109G' },
-];
+    getVolumes(): void {
+        this.volumeService.getVolumes().subscribe((volumes) => this.volumes = volumes);
+    }
+}
