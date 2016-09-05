f2cf8e24d5f7aa83780a036381c35cbf29afd5a5
Samridh <samridh@purestorage.com>
2016-09-04 18:18:50 -0700

    Part4: Services

---
+++ app/mock-volumes.ts
@@ -0,0 +1,14 @@
+import { Volume } from './volume';
+
+export const VOLUMES: Volume[] = [
+    { name: 'vol1', size: '100G' },
+    { name: 'vol2', size: '101G' },
+    { name: 'vol3', size: '102G' },
+    { name: 'vol4', size: '103G' },
+    { name: 'vol5', size: '104G' },
+    { name: 'vol6', size: '105G' },
+    { name: 'vol7', size: '106G' },
+    { name: 'vol8', size: '107G' },
+    { name: 'vol9', size: '108G' },
+    { name: 'vol10', size: '109G' },
+];
